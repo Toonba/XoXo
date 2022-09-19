d42a5734e5c068ecbbdb0f20394503cdd318ed5c
Lagniaux Jean <avaxpotass@gmail.com>
2022-09-19 22:06:11 +0200

    Update

--- onePage/onePage.js
+++
@@ -1,8 +0,0 @@
-var clickCount = 0
-document.getElementById('buy').addEventListener('click', (event)=> {
-    document.getElementById('purchaseLink').style.display = 'block';
-    clickCount++;
-    if(clickCount==2){
-        document.getElementById('purchaseLink').style.display = 'none';
-        clickCount = 0
-}})
